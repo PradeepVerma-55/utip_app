13ab13700a696603f8317fc57d601a36e6d0b20d
Pradeep Verma <pradeepnetcore@outlook.com>
2025-10-18 20:56:34 +0530

    creating a solit bill section

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class UTip extends StatefulWidget {
   const UTip({super.key});
 
@@ -31,9 +32,10 @@ class _UTipState extends State<UTip> {
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
-    final style=theme.textTheme.titleMedium!.copyWith(
+
+    final style = theme.textTheme.titleMedium!.copyWith(
       color: theme.colorScheme.onPrimary,
-      fontWeight: FontWeight.bold,  
+      fontWeight: FontWeight.bold,
     );
     return Scaffold(
       appBar: AppBar(title: const Text('UTip')),
@@ -51,16 +53,62 @@ class _UTipState extends State<UTip> {
               ),
               child: Column(
                 children: [
-                  Text(
-                    'Total Per Person!',
-                    style: style,
-                  ),
+                  Text('Total Per Person!', style: style),
                   SizedBox(height: 8),
-                  Text("\$ 23.78", 
-                  style: style.copyWith(
-                    color: theme.colorScheme.onPrimary,
-                    fontSize: theme.textTheme.displaySmall?.fontSize,
-                    )),
+                  Text(
+                    "\$ 23.78",
+                    style: style.copyWith(
+                      color: theme.colorScheme.onPrimary,
+                      fontSize: theme.textTheme.displaySmall?.fontSize,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 24),
+            Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                border: Border.all(color: theme.colorScheme.primary, width: 2),
+              ),
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.attach_money),
+                      labelText: 'Bill Amount',
+                    ),
+                    keyboardType: TextInputType.number,
+                    onChanged: (String value) {
+                      // Handle bill amount change
+                      print('Bill Amount: $value');
+                    },
+                  ),
+                  //Split the bill Area
+                  SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Split", style: theme.textTheme.titleMedium),
+                      Row(
+                        children: [
+                          IconButton(
+                            color: theme.colorScheme.primary,
+                            onPressed: () => {},
+                            icon: const Icon(Icons.remove),
+                          ),
+                          Text("2", style: theme.textTheme.titleMedium),
+                          IconButton(
+                            color: theme.colorScheme.primary,
+                            onPressed: () => {},
+                            icon: const Icon(Icons.add),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ),
